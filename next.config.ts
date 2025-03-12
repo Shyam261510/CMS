3050160fe47f4bb1a601ce19ec3a62de55414e15
shyam261510 <shyam261510@gmail.com>
2025-03-12 14:20:23 +0530

    changs in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  module.exports = {
+    env: {
+      NEXTAUTH_URL: process.env.NEXTAUTH_URL || "http://localhost:3000",
+    },
+  };
 };
 
 export default nextConfig;
